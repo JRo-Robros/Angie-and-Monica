fb3da10cdb91fb6d9ca4bf8b857ca6c8fb121ea0
Ephraim Goodenough <ephraim.goodenough@neuromatics.net>
2022-01-29 15:48:35 -0600

    Added Main Menu & Rewired Scenes

--- Scripts/Title.gd
+++ Scripts/Title.gd
@@ -2,17 +2,17 @@ extends Node2D
 
 
 func _unhandled_key_input(event):
-	if event.pressed and event.physical_scancode == 16777221:
-		load_game()
+	if event.pressed:
+		load_menu()
 
 
 func _on_TextEdit_gui_input(event) -> void:
 	if not (event is InputEventKey):
 		return
 	if event.physical_scancode == 16777221:
-		load_game()
+		load_menu()
 	return
 
-func load_game():
+func load_menu():
 	LevelsData.set_custom_level($TextEdit.text)
-	get_tree().change_scene("res://Game.tscn")
+	get_tree().change_scene("res://MainMenu.tscn")
