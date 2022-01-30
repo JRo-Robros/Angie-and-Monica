78b58aa34cae5e2e15c749f4a56bf9c5327c7098
Ephraim Goodenough <ephraim.goodenough@neuromatics.net>
2022-01-29 19:30:35 -0600

    Removed Json Parser From Title Screen

--- Scripts/Title.gd
+++ Scripts/Title.gd
@@ -6,13 +6,13 @@ func _unhandled_key_input(event):
 		load_menu()
 
 
-func _on_TextEdit_gui_input(event) -> void:
-	if not (event is InputEventKey):
-		return
-	if event.physical_scancode == 16777221:
-		load_menu()
-	return
+#func _on_TextEdit_gui_input(event) -> void:
+#	if not (event is InputEventKey):
+#		return
+#	if event.physical_scancode == 16777221:
+#		load_menu()
+#	return
 
 func load_menu():
-	LevelsData.set_custom_level($TextEdit.text)
+	#LevelsData.set_custom_level($TextEdit.text)
 	get_tree().change_scene("res://MainMenu.tscn")
